7c6bc8d0d4e700b48c0ff736cb6345930a59e7dd
naresh-vunnam77 <77763333+naresh-vunnam77@users.noreply.github.com>
2024-03-10 13:04:41 +0530

    Update TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -90,7 +90,7 @@ const TaskList = () => {
                         <p className="text-green-500 text-sm">
                           { (days > 0) && `${days} ${days === 1 ? 'day' : 'days'}` }{ ' ' }
                           { (hours > 0) && `${hours} ${hours === 1 ? 'hour' : 'hours'}` }{ ' ' }
-                          { (days === 0 && hours === 0 && minutes > 0) && `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}` } left
+                          { (days === 0 && hours === 0 && minutes > 0) && `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}` } time left
                           { (days === 0 && hours === 0 && minutes === 0) && 'Passed due date' }
                         </p>
 
