3deae3594ed9277bacaee411ef6fc54a266ed9bf
naresh-vunnam77 <77763333+naresh-vunnam77@users.noreply.github.com>
2024-03-12 19:57:52 +0530

    Create Util functions
    Create Util functions

---
+++ src/utils/util.js
@@ -0,0 +1,17 @@
+export const calculateTimeRemaining = (dueDate) => {
+  const now = new Date();
+  const difference = dueDate - now;
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  return { days, hours, minutes };
+};
+
+export const calculateElapsedTime = (dueDate) => {
+  const now = new Date();
+  const difference = now - dueDate;
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  return { days, hours, minutes };
+};
